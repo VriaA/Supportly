c7afcf6e532e37d84afcfe441732a625161bde1f
VriaA <123843734+VriaA@users.noreply.github.com>
2024-08-07 08:02:01 +0100

    setup prettier for consistent code formatting

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
-import { Inter } from "next/font/google"
-import "./globals.css"
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Chat bot",
   description:
     "An AI-powered chatbot designed to handle customer support queries.",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
