410c71eac986ccb7c731e5310edba7fb21a52064
VriaA <123843734+VriaA@users.noreply.github.com>
2024-08-09 19:13:59 +0100

    Implement messaging feature

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
-import styles from "./page.module.css";
+import { Container } from "@mui/material";
+import Messages from "@/components/Messages";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <Container>
+      <Messages />
+    </Container>
+  );
 }
---
+++ components/Messages.js
@@ -0,0 +1,69 @@
+"use client";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import useMessage from "@/hooks/useMessage";
+import { Fragment } from "react";
+export default function Messages() {
+  const { setMessage, sendMessage, messages, message } = useMessage();
+
+  return (
+    <Stack
+      direction={"column"}
+      width="500px"
+      height="100svh"
+      p={2}
+      spacing={3}
+    >
+      <Stack
+        direction={"column"}
+        spacing={2}
+        flexGrow={1}
+        overflow="auto"
+        maxHeight="100%"
+      >
+        {messages.map((message, index) => (
+          <Box
+            key={`message-${index + 1}`}
+            display="flex"
+            justifyContent={
+              message.role === "assistant" ? "flex-start" : "flex-end"
+            }
+          >
+            <Box
+              bgcolor={
+                message.role === "assistant" ? "primary.main" : "secondary.main"
+              }
+              color="white"
+              borderRadius="16px"
+              padding="8px 16px"
+            >
+              {message.content.split(/\d+\.\s*/).map((sentence, index) => (
+                <Fragment key={`message-${index + 1}`}>
+                  {index !== 0 && <br />}
+                  <Typography>{sentence}</Typography>
+                </Fragment>
+              ))}
+            </Box>
+          </Box>
+        ))}
+      </Stack>
+
+      <Stack
+        direction={"row"}
+        spacing={2}
+      >
+        <TextField
+          label="Message"
+          fullWidth
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          onClick={sendMessage}
+        >
+          Send
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
